e8d34381f5f05fb77c2083bd7c7063ec4d498530
MilaBortnik <mila_bortnik_1803@ukr.net>
2022-02-03 21:54:56 +0200

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
-const gulp = require('gulp');
-const terser = require('gulp-terser');
+const { src, dest } = require('gulp')
+const terser = require('gulp-terser')
+const sourcemaps = require('gulp-sourcemaps')
+const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const plumber = require('gulp-plumber')
 
-const html = function(){
-    return gulp.src('./source/index.*html')
-    .pipe(gulp.dest('./build/'));
+
+ html = function(){
+    return src('./source/*.html')
+    .pipe(dest('./build/'));
 }
 
-const js = function(){
-    return gulp.src('./sourse/js/scrip.js')
+    js = function(){
+    return src('./sourse/js/scrip.js')
     .pipe(terser())
-    .pipe(gulp.dest('./build'))
+    .pipe(dest('./build'))
+}
+    css = function(){
+    return src('./source/sass/**/*.sass')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemaps.write("./"))
+    .pipe(dest('./build/css'))
 }
 
 exports.js = js
 exports.html = html
+exports.css = css
